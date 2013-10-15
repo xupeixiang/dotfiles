6427ae3d5a368157e11005f771104c430064b0e2
Peixiang xu <peixiangxu@gmail.com>
2013-10-15 19:36:19 +0800

    delete shell script

--- diff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dotfiles_path=~/projects/dotfiles
-
-diff $dotfiles_path/.gitconfig ~/.gitconfig 
-diff $dotfiles_path/.bashrc ~/.bashrc
-diff $dotfiles_path/.vimrc ~/.vimrc 
-
-cd $dotfiles_path
-git st
